770fbc986d2e1355ca9c5275874a620e9a3b0f22
unknown <praneeth.vadrevu@gmail.com>
2024-03-06 22:24:40 +0530

    created a create-react-app without using the command but by manually installing npm and parcel

--- namasteReact1/app.js
+++ namasteReact1/app.js
@@ -1,15 +1,10 @@
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>I am an H1 tag.</h1>
-        <h2>I am an H2 tag.</h2>
-    </div>
-</div> */
-}
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
 
-const heading = React.createElement("h1", { id: "parent" }, [
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am an H1 tag."),
     React.createElement("h2", {}, "I am an H2 tag."),
@@ -19,6 +14,6 @@ const heading = React.createElement("h1", { id: "parent" }, [
     React.createElement("h2", {}, "I am an H2 tag."),
   ]),
 ]);
-console.log(heading);
+console.log(parent);
 
-root.render(heading);
+root.render(parent);
