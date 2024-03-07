8d235d9347d0c6d4155de3b954762ea92b5d2d67
unknown <praneeth.vadrevu@gmail.com>
2024-03-07 22:29:31 +0530

    JSX and function based components)

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// react element
+const heading = (
+  <h1 className="heading" id="heading">
+  
+    This is H1 via JSX
+  </h1>
+);
+
+// functional component
+const HeadingComponent = () => {
+  return <h1 className="heading">This is a functional component</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
